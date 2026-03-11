b20fb669f56de75cd1358d63d14376e2983fae38
samenoko-dayo <kumagai7554@gmail.com>
2026-03-11 09:52:48 +0900

    remark-breaksの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import remarkBreaks from "remark-breaks";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    remarkPlugins: [remarkBreaks]
   },
   image: {
     domains: ["imag.samenoko.work"],
