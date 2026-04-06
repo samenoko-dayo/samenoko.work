8c87fb8bcbef09a61afbc3f519845f510fe8a2bb
samenoko-dayo <kumagai7554@gmail.com>
2026-04-06 22:42:34 +0900

    コードブロックの強化

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,23 @@ import tailwindcss from "@tailwindcss/vite";
 import remarkBreaks from "remark-breaks";
 import { remarkBlogCard } from "./src/lib/remark-blog-card.mjs";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     remarkPlugins: [remarkBlogCard,remarkBreaks]
   },
+
   image: {
     domains: ["imag.samenoko.work"],
     remotePatterns: [{ protocol: "https" }]
   },
-  site: "https://blog.samenoko.work"
+
+  site: "https://blog.samenoko.work",
+  integrations: [expressiveCode()]
 });
